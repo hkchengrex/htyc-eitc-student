6bca15654358ed1b1678a59be5a1dd20f88530e7
fung <funghiantie@gmail.com>
2018-04-06 23:35:15 +0800

    Add class1ex3

---
+++ class1ex3/src/edu/eitc/GoodNamePrinter.java
@@ -0,0 +1,13 @@
+package edu.eitc;
+
+public class GoodNamePrinter {
+    String name;
+
+    void setName(String inputName){
+        name = inputName;
+    }
+
+    void printName(){
+        System.out.println(name);
+    }
+}
---
+++ class1ex3/src/edu/eitc/Main.java
@@ -0,0 +1,11 @@
+package edu.eitc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GoodNamePrinter printer = new GoodNamePrinter();
+
+        printer.setName("Peter");
+        printer.printName();
+    }
+}
